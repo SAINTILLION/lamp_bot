5c92ad7e2e2759afad595035cf96587473cf5da9
SAINTILLION <ebolommcsaint@gmail.com>
2024-08-28 13:42:22 +0100

    added the glow to the microphone

--- lib/presentation/flashlight.dart
+++ lib/presentation/flashlight.dart
@@ -1,4 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
 
 class FlashlightScreen extends StatefulWidget {
   const FlashlightScreen({super.key});
@@ -9,6 +12,16 @@ class FlashlightScreen extends StatefulWidget {
 
 class _FlashlightScreenState extends State<FlashlightScreen> {
   bool isFlashlightOn = false; //This means the light is off
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  bool isMicOn = false;
+  
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
     
@@ -23,17 +36,30 @@ class _FlashlightScreenState extends State<FlashlightScreen> {
           alignment: Alignment.topRight,
           child: InkWell(
             onTap: () {
-              //Add functionality for speeh recognition
+              //TODO: Add functionality for speech recognition
+             setState(() {
+               isMicOn = !isMicOn;
+             }); 
             },
-            child: Container(
-               decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color:isFlashlightOn == false ? Colors.white : Colors.black,  // Border color
-                  width: 2.0, // Border width
+            child: AvatarGlow(
+              glowColor: Colors.white,
+              glowShape: BoxShape.circle,
+              glowRadiusFactor: 3,
+              animate: true,
+              startDelay: Duration.zero,
+              duration: const Duration(milliseconds: 2000),
+              glowCount: 2,
+              child: Container(
+                 decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color:isFlashlightOn == false ? Colors.white : Colors.black,  // Border color
+                    width: 2.0, // Border width
+                  ),
                 ),
+                child: const Icon(Icons.mic, size: 50,)
               ),
-              child: const Icon(Icons.mic, size: 50,)
             )
           ),
         ),
@@ -53,7 +79,7 @@ class _FlashlightScreenState extends State<FlashlightScreen> {
               IconButton(
                 icon: Icon(
                   isFlashlightOn == false ? Icons.flashlight_off_outlined : Icons.flashlight_on, 
-                  color: Colors.blue,
+                  color: isFlashlightOn == false ? Colors.white : Colors.blue,
                   size: 300,
                 ),
                 onPressed: () {
